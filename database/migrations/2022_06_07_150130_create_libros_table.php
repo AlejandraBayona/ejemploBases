6ab18e99729f1f0220ff39ef071e31a1c8f099fa
Alejandra Canizalez <cs11004@ues.edu.sv>
2022-06-07 10:18:41 -0600

    commit1

---
+++ database/migrations/2022_06_07_150130_create_libros_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo',150);
+            $table->integer('autor_id',null,true)->unsigned()->index();
+
+            $table->foreign('autor_id')
+            ->references('id')
+            ->on('autors')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
